dbda4248dfbaafd74fe9b24a81f3d8088076170d
Trent Peterson <tpeterson998@west-mec.org>
2019-02-05 15:19:22 -0700

    Day 1
    We set up a alert notification then commented it out

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,12 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
-
+   Author: Trent Peterson
+   Date:   2.5.19  
 */
 
+// window.alert("Welcome to Tulsa");
